399c508f96e3e20dbb33a1872d036231dec37c68
Fred <terrage.frederic@orange.fr>
2017-11-19 13:53:15 +0100

    Factory.java fabrique les objets, simplification de Main

---
+++ Factory.java
@@ -0,0 +1,27 @@
+
+package main;
+
+public class Factory {
+
+	public static Mode get(char game, char choice) {
+
+		Mode mode = null;
+
+		if (game == '1' && choice == '1') {
+			mode = new ChallengerR();
+		} else if (game == '1' && choice == '2') {
+			mode = new DefenserR();
+		} else if (game == '1' && choice == '3') {
+			mode = new DuelR();
+		} else if (game == '2' && choice == '1') {
+			mode = new ChallengerM();
+		} else if (game == '2' && choice == '2') {
+			mode = new DefenserM();
+		} else if (game == '2' && choice == '3') {
+			mode = new DuelM();
+		} else if (game == '1' && choice == '4') {
+			mode = new Developer();
+		}
+		return mode;
+	}
+}
