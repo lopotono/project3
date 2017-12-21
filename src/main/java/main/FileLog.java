a401ea318c6dfada47a302f15edade4e52eeddca
lopotono <30410435+lopotono@users.noreply.github.com>
2017-12-21 15:33:50 +0100

    Add files via upload

--- src/main/java/main/FileLog.java
+++ src/main/java/main/FileLog.java
@@ -1,24 +1,14 @@
-package main;
+package p3;
 
-import java.io.IOException;
-
-import org.apache.log4j.FileAppender;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
+import org.apache.log4j.xml.DOMConfigurator;
 
 public class FileLog {
 
-	private static Logger logger = Logger.getLogger(Logger.class);
 
 	public void file() {
-
-		try {
-			FileAppender fi = new FileAppender(new PatternLayout(), "C://Users/Fred/Desktop/logs.txt");
-			fi.setName("fileLog");
-			logger.addAppender(fi);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+				
+		String logs = System.getProperty("user.dir") + "\\" + "p3\resources\\logs.txt";
+		DOMConfigurator.configure(logs);
+		
 	}
 }
